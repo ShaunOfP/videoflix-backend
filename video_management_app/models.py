a52779b22354b084f7a5dcfe115337ad9dca76ae
ShaunOfP <131476113+ShaunOfP@users.noreply.github.com>
2026-05-18 17:13:41 +0200

    added default thumbnail

--- video_management_app/models.py
+++ video_management_app/models.py
@@ -6,7 +6,7 @@ class Video(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=255)
     description = models.TextField()
-    thumbnail_url = models.URLField(max_length=500)
+    thumbnail_url = models.URLField(max_length=500, default='none')
     category = models.CharField(max_length=255, default='Uncategorized')
     video_file = models.FileField(upload_to='videos/')
 
